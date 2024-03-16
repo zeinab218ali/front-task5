57361e471d3c3563d6698285a8828a8147ce4276
unknown <zeinab.218.ali@gmail.com>
2024-03-16 06:23:54 +0300

    video view css

--- src/components/VideoView/VideoView.jsx
+++ src/components/VideoView/VideoView.jsx
@@ -1,22 +1,55 @@
 import './VideoView.css'
-import Hero from './components/Hero/Hero'
+import Hero from '../Hero/Hero'
 import { FaPlayCircle } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
 const VideoView = () => {
     return (
         <div className='video-container'>
-        <Hero photo='./video-bg' sp='| VIDEO VIEW' H3='Get Closer View & Different Feeling' />
-        <img src="./video-frame.jpg" alt="" />
-        <div className="link-container">
-        <NavLink
+            <Hero photo='./video-bg.jpg' sp='| VIDEO VIEW' H3='Get Closer View & Different Feeling' />
+            <img className='video-img' src="./video-frame.jpg" alt="" />
+            <div className="link-container">
+                <NavLink
                     to={'https://www.youtube.com/'}
 
                 >
-                    <div className="icon">
-                    <FaPlayCircle />
+                    <div className="icon-container">
+                        <div className="icon">
+                            <FaPlayCircle color='#f35525' />
+                        </div>
                     </div>
                 </NavLink>
-        </div>
+            </div>
+            <div className="mini-cards-container">
+                <div className="card">
+                    <div className="mini-card">
+                        <h2>34</h2>
+                        <p> Buildings
+                            Finished Now
+                        </p>
+                    </div>
+                    <div className="circle"></div>
+                </div>
+                <div className="card">
+                    <div className="mini-card">
+                        <h2>12</h2>
+                        <p> Years
+                            Experience
+                        </p>
+                    </div>
+                    <div className="circle"></div>
+                </div>
+                <div className="card">
+                    <div className="mini-card">
+                        <h2>24</h2>
+                        <p> Awwards
+                            Won 2023
+                        </p>
+                    </div>
+                    <div className="circle"></div>
+                </div>
+
+            </div>
         </div>
     )
 }
